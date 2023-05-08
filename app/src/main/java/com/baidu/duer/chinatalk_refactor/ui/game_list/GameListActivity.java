a103d90965dd366270d56f64fd3bd7c271941ce5
wzc520pyfm <1528857653@qq.com>
2023-05-08 10:42:09 +0800

    feat: add game list activity

---
+++ app/src/main/java/com/baidu/duer/chinatalk_refactor/ui/game_list/GameListActivity.java
@@ -0,0 +1,16 @@
+package com.baidu.duer.chinatalk_refactor.ui.game_list;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.baidu.duer.chinatalk_refactor.R;
+
+public class GameListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_list);
+    }
+}
